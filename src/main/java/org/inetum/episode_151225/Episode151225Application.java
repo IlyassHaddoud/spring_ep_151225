c604755f6122bff5f19dbfb3696967972915676f
Ilyass HADDOUD <ilyass.haddoud@inetum.com>
2025-12-15 19:59:42 +0100

    15/12/2025

---
+++ src/main/java/org/inetum/episode_151225/Episode151225Application.java
@@ -0,0 +1,13 @@
+package org.inetum.episode_151225;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Episode151225Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Episode151225Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/inetum/episode_151225/Episode151225ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inetum.episode_151225;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Episode151225ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
